dcf939ac19d5ec92d67f0e8689b1188a3b82f3f7
Shrijan Regmi <your.email@example.com>
2023-10-05 09:59:21 +0545

    feat: added PPMori fonts

--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class AppTheme {
   /// Provides the light theme for the application.
   static ThemeData get lightThemePalette {
-    return ThemeData();
+    return ThemeData(
+      fontFamily: 'PPMori',
+    );
   }
 
   /// Provides the dark theme for the application.
